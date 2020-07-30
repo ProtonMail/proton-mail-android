24b4f5f6f44ad17551f5562cb7aaf9f049e5dd53
Davide Farella <4face91@protonmail.com>
2020-07-30 14:32:27 +0200

    Removed injectLicenses Gradle task
    #comment Transformed InjectLicenses.gradle.kts into plain script InjectLicenses.kts.
    Affected: Copyright injection during Gradle sync, it is now handled by the IDE itself

--- scripts/inject_licenses/InjectLicenses.kts
+++ scripts/inject_licenses/InjectLicenses.kts
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2020 Proton Technologies AG
- * 
+ *
  * This file is part of ProtonMail.
- * 
+ *
  * ProtonMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ProtonMail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
  */
@@ -22,6 +22,8 @@ import java.io.File
 import java.io.IOException
 
 /*
+ * Run by `` kscript scripts/inject_licenses/InjectLicenses.kts `` into your CLI / terminal
+ *
  * This script will Inject the license header for all the source files who are not provided with it
  * @author Davide Farella
  */
