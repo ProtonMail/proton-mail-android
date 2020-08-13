f3d762b4716f7d35226b552d6f102e5a4e7467ca
Davide Farella <4face91@protonmail.com>
2020-08-13 10:46:10 +0200

    EmailAddress.VALIDATION_REGEX to be case insensitive
    Converted `EmailAddress.VALIDATION_REGEX` from String to Regex and included `RegexOption.IGNORE_CASE`
    Affected: Email validation
    
    MAILAND-830

--- app/src/main/java/ch/protonmail/android/utils/extensions/CommonExtensions.kt
+++ app/src/main/java/ch/protonmail/android/utils/extensions/CommonExtensions.kt
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2020 Proton Technologies AG
- * 
+ *
  * This file is part of ProtonMail.
- * 
+ *
  * ProtonMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ProtonMail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
  */
@@ -26,10 +26,8 @@ import androidx.lifecycle.MediatorLiveData
 import ch.protonmail.android.api.models.ResponseBody
 import ch.protonmail.android.core.ProtonMailApplication
 import ch.protonmail.android.domain.entity.EmailAddress
-import ch.protonmail.android.utils.MessageUtils
 import com.google.gson.Gson
 import retrofit2.HttpException
-import java.util.regex.Pattern
 
 fun HttpException.toPMResponseBody(): ResponseBody? {
     return response()?.errorBody()?.toPMResponseBody()
@@ -68,6 +66,6 @@ fun Uri.isEmpty() = this == Uri.EMPTY
  */
 val <T : Any?> T.exhaustive get() = this
 
-fun String.isValidEmail(): Boolean = Pattern.compile(EmailAddress.VALIDATION_REGEX).matcher(this).matches()
+fun String.isValidEmail(): Boolean = EmailAddress.VALIDATION_REGEX.matches(this)
 
 val Context.app get() = applicationContext as ProtonMailApplication
