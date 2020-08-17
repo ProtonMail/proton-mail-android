0cb81a736e1ce9d2c970399e5c61f46ce0334327
Davide Farella <4face91@protonmail.com>
2020-08-17 16:00:08 +0200

    Deprecated AnyExtensions.kt
    Deprecated with descriptive message and replacements
    Affected: none

--- app/src/main/java/ch/protonmail/android/utils/extensions/AnyExtensions.kt
+++ app/src/main/java/ch/protonmail/android/utils/extensions/AnyExtensions.kt
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2020 Proton Technologies AG
- * 
+ *
  * This file is part of ProtonMail.
- * 
+ *
  * ProtonMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ProtonMail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
  */
@@ -24,12 +24,20 @@ package ch.protonmail.android.utils.extensions
 /**
  * Executes code block if the object is null
  */
+@Deprecated(
+    DEPRECATION_MESSAGE,
+    ReplaceWith("if (this == null) block")
+)
 infix fun <T> T?.ifNull(block: () -> Unit) {
     if (this == null) {
         block()
     }
 }
 
+@Deprecated(
+    DEPRECATION_MESSAGE,
+    ReplaceWith("if (this == null) blockNull else blockElse")
+)
 fun <T> T?.ifNullElse(blockNull: () -> Unit, blockElse: () -> Unit) {
     if (this == null) {
         blockNull()
@@ -38,6 +46,10 @@ fun <T> T?.ifNullElse(blockNull: () -> Unit, blockElse: () -> Unit) {
     }
 }
 
+@Deprecated(
+    DEPRECATION_MESSAGE,
+    ReplaceWith("if (this == null) blockNull else blockElse")
+)
 fun <T, U> T?.ifNullElseReturn(blockNull: () -> U, blockElse: (T) -> U): U {
     return if (this == null) {
         blockNull()
@@ -46,6 +58,10 @@ fun <T, U> T?.ifNullElseReturn(blockNull: () -> U, blockElse: (T) -> U): U {
     }
 }
 
+@Deprecated(
+    DEPRECATION_MESSAGE,
+    ReplaceWith("if (this.isEmpty()) blockEmpty else blockElse")
+)
 fun <T> List<T>?.ifEmptyElse(blockEmpty: () -> Unit, blockElse: () -> Unit) {
     if (this == null || size == 0) {
         blockEmpty()
@@ -54,8 +70,15 @@ fun <T> List<T>?.ifEmptyElse(blockEmpty: () -> Unit, blockElse: () -> Unit) {
     }
 }
 
+@Deprecated(
+    DEPRECATION_MESSAGE,
+    ReplaceWith("requireNotNull(this)")
+)
 infix fun <T> T?.notNull(variableName: String): T {
     if (this == null)
         throw RuntimeException("$variableName is null")
     return this
 }
+
+private const val DEPRECATION_MESSAGE = "This function is deprecate: It makes the code harder to be read, " +
+    "instantiate new objects ( noinline lambdas ) and inhibit the smart-cast where it can be applied"
