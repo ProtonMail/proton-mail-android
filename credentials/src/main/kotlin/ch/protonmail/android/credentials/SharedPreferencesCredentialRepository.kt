6e7e61e63eb2c7aa89832e13354a3195a905e778
Davide Farella <4face91@protonmail.com>
2020-09-01 09:49:17 +0200

    Create SharedPreferencesCredentialRepository.kt
    With relative SharedPreferencesCredentialRepositoryTest.kt
    Affected: none
    
    MAILAND-896

---
+++ credentials/src/main/kotlin/ch/protonmail/android/credentials/SharedPreferencesCredentialRepository.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ *
+ * This file is part of ProtonMail.
+ *
+ * ProtonMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+ */
+
+package ch.protonmail.android.credentials
+
+import android.content.SharedPreferences
+import ch.protonmail.android.credentials.Credential.NotFound
+import ch.protonmail.android.domain.entity.EmailAddress
+import me.proton.core.util.android.sharedpreferences.get
+import me.proton.core.util.android.sharedpreferences.minusAssign
+import me.proton.core.util.android.sharedpreferences.set
+import me.proton.core.util.kotlin.deserialize
+
+internal class SharedPreferencesCredentialRepository(
+    private val preferences: SharedPreferences
+) {
+
+    fun getAll(): Map<EmailAddress, Credential> =
+        preferences.all.map { (k, v) ->
+            EmailAddress(k) to (v as String).deserialize(Credential.serializer())
+        }.toMap()
+
+    operator fun get(address: EmailAddress): Credential =
+        preferences[address.s] ?: NotFound
+
+    operator fun set(address: EmailAddress, credential: Credential) {
+        require(credential != NotFound) { "Cannot set 'NotFound' as credential" }
+        preferences[address.s] = credential
+    }
+
+    operator fun minusAssign(address: EmailAddress) {
+        preferences -= address.s
+    }
+}
