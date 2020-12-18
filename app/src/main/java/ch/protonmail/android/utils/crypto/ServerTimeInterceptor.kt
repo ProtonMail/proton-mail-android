e3bb90468b401964c86e7e7b924b6c51933e25b9
Dimitar Solev <dimitar.solev@protonmail.com>
2020-12-18 08:57:08 +0100

    Fix for app crashing due to an ArrayIndexOutOfBoundsException in ServerTimeInterceptor.kt
    Affected: ServerTimeInterceptor.kt
    
    MAILAND-1273

--- app/src/main/java/ch/protonmail/android/utils/crypto/ServerTimeInterceptor.kt
+++ app/src/main/java/ch/protonmail/android/utils/crypto/ServerTimeInterceptor.kt
@@ -18,6 +18,7 @@
  */
 package ch.protonmail.android.utils.crypto
 
+import android.os.Build
 import ch.protonmail.android.core.QueueNetworkUtil
 import ch.protonmail.android.utils.ServerTime
 import okhttp3.Interceptor
@@ -26,6 +27,9 @@ import timber.log.Timber
 import java.io.IOException
 import java.text.ParseException
 import java.text.SimpleDateFormat
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.Locale
 import java.util.TimeZone
 
@@ -54,12 +58,22 @@ class ServerTimeInterceptor(
     private fun handleResponse(response: Response) {
         val dateString = response.header("date", null) ?: return
         try {
-            val rfc1123Format = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US)
-            rfc1123Format.timeZone = TimeZone.getTimeZone("GMT")
-            val date = rfc1123Format.parse(dateString)
-            date?.let {
-                openPgp.updateTime(date.time / 1000)
-                ServerTime.updateServerTime(date.time)
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val parsedInstant = Instant.from(DateTimeFormatter.RFC_1123_DATE_TIME.parse(dateString))
+                if (parsedInstant != null) {
+                    val epochTimeSeconds = parsedInstant.atZone(ZoneId.systemDefault()).toEpochSecond()
+                    openPgp.updateTime(epochTimeSeconds)
+                    ServerTime.updateServerTime(epochTimeSeconds * 1000)
+                }
+            } else {
+                val rfc1123Format = SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss z", Locale.US)
+                rfc1123Format.timeZone = TimeZone.getTimeZone("GMT")
+                val date = rfc1123Format.parse(dateString)
+                if (date != null) {
+                    openPgp.updateTime(date.time / 1000)
+                    ServerTime.updateServerTime(date.time)
+                }
             }
         } catch (exception: ParseException) {
             Timber.w(exception, "Date parse exception")
