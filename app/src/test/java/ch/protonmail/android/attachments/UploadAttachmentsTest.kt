ProtonMail/proton-mail-android

commit 6319af6bef33122b94266d28557c6cd57c7b5111

SaveDraft use case uploads attachments through UploadAttachments use case
This is not a complete commit, it's just to store the current state. As
the existing `PostCreateDraftAttachmentsJob` does more than what
`UploadAttachments` use case is doing, some changes are pending

MAILAND-1018

app/src/main/java/ch/protonmail/android/usecase/compose/SaveDraft.kt
@@ -24,6 +24,7 @@ import ch.protonmail.android.activities.messageDetails.repository.MessageDetails
 import ch.protonmail.android.api.models.room.messages.Message
 import ch.protonmail.android.api.models.room.pendingActions.PendingActionsDao
 import ch.protonmail.android.api.models.room.pendingActions.PendingUpload
+import ch.protonmail.android.attachments.UploadAttachments
 import ch.protonmail.android.core.Constants
 import ch.protonmail.android.core.Constants.MessageLocationType.ALL_DRAFT
 import ch.protonmail.android.core.Constants.MessageLocationType.ALL_MAIL
@@ -48,7 +49,8 @@ class SaveDraft @Inject constructor(
     private val dispatchers: DispatcherProvider,
     private val pendingActionsDao: PendingActionsDao,
     private val createDraftWorker: CreateDraftWorker.Enqueuer,
-    @CurrentUsername private val username: String
+    @CurrentUsername private val username: String,
+    val uploadAttachments: UploadAttachments
 ) {
 
     suspend operator fun invoke(
@@ -90,23 +92,20 @@ class SaveDraft @Inject constructor(
         )
             .filter { it.state.isFinished }
             .map { workInfo ->
-                return@map handleWorkResult(workInfo, messageId)
+                val workSucceeded = workInfo.state == WorkInfo.State.SUCCEEDED
+
+                if (workSucceeded) {
+                    val createdDraftId = workInfo.outputData.getString(KEY_OUTPUT_DATA_CREATE_DRAFT_RESULT_MESSAGE_ID)
+                    updatePendingForSendMessage(createdDraftId, messageId)
+                    deleteOfflineDraft(messageId)
+                    uploadAttachments(params.newAttachmentIds, message, addressCrypto)
+
+                    return@map Result.Success
+                }
+
+                return@map Result.Success
             }
 
-    }
-
-    private fun handleWorkResult(workInfo: WorkInfo, localDraftId: String): Result {
-        val workSucceeded = workInfo.state == WorkInfo.State.SUCCEEDED
-
-        if (workSucceeded) {
-            val createdDraftId = workInfo.outputData.getString(KEY_OUTPUT_DATA_CREATE_DRAFT_RESULT_MESSAGE_ID)
-            updatePendingForSendMessage(createdDraftId, localDraftId)
-            deleteOfflineDraft(localDraftId)
-
-            return Result.Success
-        }
-
-        return Result.Success
     }
 
     private fun deleteOfflineDraft(localDraftId: String) {

app/src/test/java/ch/protonmail/android/usecase/compose/SaveDraftTest.kt
@@ -27,6 +27,7 @@ import ch.protonmail.android.api.models.room.messages.Message
 import ch.protonmail.android.api.models.room.pendingActions.PendingActionsDao
 import ch.protonmail.android.api.models.room.pendingActions.PendingSend
 import ch.protonmail.android.api.models.room.pendingActions.PendingUpload
+import ch.protonmail.android.attachments.UploadAttachments
 import ch.protonmail.android.core.Constants.MessageActionType.FORWARD
 import ch.protonmail.android.core.Constants.MessageActionType.REPLY
 import ch.protonmail.android.core.Constants.MessageActionType.REPLY_ALL
@@ -60,6 +61,9 @@ import java.util.UUID
 
 @ExtendWith(MockKExtension::class)
 class SaveDraftTest : CoroutinesTest {
+
+    @RelaxedMockK
+    private lateinit var uploadAttachments: UploadAttachments
 
     @RelaxedMockK
     private lateinit var createDraftScheduler: CreateDraftWorker.Enqueuer
@@ -311,6 +315,48 @@ class SaveDraftTest : CoroutinesTest {
             verify { messageDetailsRepository.deleteMessage(message) }
         }
 
+    @Test
+    fun saveDraftsCallsUploadAttachmentsUseCaseToUploadNewAttachments() =
+        runBlockingTest {
+            // Given
+            val localDraftId = "8345"
+            val message = Message().apply {
+                dbId = 123L
+                this.messageId = "45623"
+                addressID = "addressId"
+                decryptedBody = "Message body in plain text"
+                localId = localDraftId
+            }
+            val workOutputData = workDataOf(
+                KEY_OUTPUT_DATA_CREATE_DRAFT_RESULT_MESSAGE_ID to "createdDraftMessageId345"
+            )
+            val workerStatusFlow = buildCreateDraftWorkerResponse(WorkInfo.State.SUCCEEDED, workOutputData)
+            val newAttachmentIds = listOf("2345", "453")
+            coEvery { messageDetailsRepository.saveMessageLocally(message) } returns 9833L
+            every { messageDetailsRepository.findMessageById("45623") } returns message
+            every { pendingActionsDao.findPendingSendByDbId(9833L) } returns null
+            every { pendingActionsDao.findPendingSendByOfflineMessageId(localDraftId) } returns PendingSend()
+            every {
+                createDraftScheduler.enqueue(
+                    message,
+                    "parentId234",
+                    REPLY_ALL,
+                    "previousSenderId132423"
+                )
+            } answers { workerStatusFlow }
+            val addressCrypto = mockk<AddressCrypto>(relaxed = true)
+            every { addressCryptoFactory.create(Id("addressId"), Name(currentUsername)) } returns addressCrypto
+
+            // When
+            saveDraft.invoke(
+                SaveDraftParameters(message, newAttachmentIds, "parentId234", REPLY_ALL, "previousSenderId132423")
+            ).first()
+
+            // Then
+            coVerify { uploadAttachments(newAttachmentIds, message, addressCrypto) }
+        }
+
+   
     private fun buildCreateDraftWorkerResponse(
         endState: WorkInfo.State,
         outputData: Data? = workDataOf()

app/src/main/java/ch/protonmail/android/worker/CreateDraftWorker.kt
@@ -121,6 +121,18 @@ class CreateDraftWorker @WorkerInject constructor(
         return Result.success(
             workDataOf(KEY_OUTPUT_DATA_CREATE_DRAFT_RESULT_MESSAGE_ID to response.messageId)
         )
+
+        // TODO test whether this is needed, drop otherwise
+// set inline attachments from parent message that were inline previously
+//        for (Attachment atta : draftMessage.getAttachments()) {
+//            if (parentAttachmentList != null && !parentAttachmentList.isEmpty()) {
+//                for (Attachment parentAtta : parentAttachmentList) {
+//                    if (parentAtta.getKeyPackets().equals(atta.getKeyPackets())) {
+//                        atta.setInline(parentAtta.getInline());
+//                    }
+//                }
+//            }
+//        }
     }
 
     private fun buildDraftRequestParentAttachments(

commit cf23850da42f8d9c84632e7438b9f4781a324ead

Upload Public key to the message only when sending it
This solves an issue for which the public key was uploaded as part of
saving a draft, each time it was saved.

This issue was introduced with commit 6319af6bef33122b94266d, which began re-using
the UploadAttachments use case logic without considering that while such logic
does indeed upload attachments, it was extracted from the sending process
(MAILAND-1139) and it contains some op (namely, upload of oublic key)
that are expected to be done only while sending and not when saving a draft.

MAILAND-1511

app/src/main/java/ch/protonmail/android/attachments/UploadAttachments.kt
@@ -44,12 +44,12 @@ class UploadAttachments @Inject constructor(
      * Use #UploadAttachments.invoke instead
      */
     @Deprecated("Needed to replace existing logic in legacy java jobs", ReplaceWith("invoke()", ""))
-    fun blocking(attachmentIds: List<String>, message: Message, crypto: AddressCrypto) =
+    fun blocking(attachmentIds: List<String>, message: Message, crypto: AddressCrypto, isMessageSending: Boolean) =
         runBlocking {
-            invoke(attachmentIds, message, crypto)
+            invoke(attachmentIds, message, crypto, isMessageSending)
         }
 
-    suspend operator fun invoke(attachmentIds: List<String>, message: Message, crypto: AddressCrypto): Result =
+    suspend operator fun invoke(attachmentIds: List<String>, message: Message, crypto: AddressCrypto, isMessageSending: Boolean): Result =
         withContext(dispatchers.Io) {
             val messageId = requireNotNull(message.messageId)
             Timber.i("UploadAttachments started for messageId $messageId - attachmentIds $attachmentIds")
@@ -91,7 +91,7 @@ class UploadAttachments @Inject constructor(
             }
 
             val isAttachPublicKey = userManager.getMailSettings(userManager.username)?.getAttachPublicKey() ?: false
-            if (isAttachPublicKey) {
+            if (isAttachPublicKey && isMessageSending) {
                 Timber.i("UploadAttachments attaching publicKey for messageId $messageId")
                 val result = attachmentsRepository.uploadPublicKey(message, crypto)
 

app/src/main/java/ch/protonmail/android/jobs/messages/PostMessageJob.java
@@ -246,7 +246,7 @@ public class PostMessageJob extends ProtonMailBaseJob {
         MailSettings mailSettings = getUserManager().getMailSettings(mUsername);
 
         UploadAttachments uploadAttachments = buildUploadAttachmentsUseCase(pendingActionsDatabase);
-        UploadAttachments.Result result = uploadAttachments.blocking(mNewAttachments, message, crypto);
+        UploadAttachments.Result result = uploadAttachments.blocking(mNewAttachments, message, crypto, true);
 
         if (result instanceof UploadAttachments.Result.Failure) {
             UploadAttachments.Result.Failure failureResult = (UploadAttachments.Result.Failure) result;

app/src/main/java/ch/protonmail/android/usecase/compose/SaveDraft.kt
@@ -104,7 +104,7 @@ class SaveDraft @Inject constructor(
                     updatePendingForSendMessage(createdDraftId, localDraftId)
 
                     messageDetailsRepository.findMessageById(createdDraftId)?.let {
-                        val uploadResult = uploadAttachments(params.newAttachmentIds, it, addressCrypto)
+                        val uploadResult = uploadAttachments(params.newAttachmentIds, it, addressCrypto, false)
 
                         if (uploadResult is UploadAttachments.Result.Failure) {
                             errorNotifier.showPersistentError(uploadResult.error, localDraft.subject)

app/src/test/java/ch/protonmail/android/attachments/UploadAttachmentsTest.kt
@@ -78,7 +78,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             val messageId = "message-id-238237"
             val message = Message(messageId)
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             val pendingUpload = PendingUpload(messageId)
             verify { pendingActionsDao.insertPendingForUpload(pendingUpload) }
@@ -93,7 +93,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             val message = Message(messageId)
             every { pendingActionsDao.findPendingUploadByMessageId(messageId) } returns PendingUpload(messageId)
 
-            val result = uploadAttachments(attachmentIds, message, crypto)
+            val result = uploadAttachments(attachmentIds, message, crypto, false)
 
             verify(exactly = 0) { pendingActionsDao.insertPendingForUpload(any()) }
             assertEquals(UploadAttachments.Result.UploadInProgress, result)
@@ -120,7 +120,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             every { messageDetailsRepository.findAttachmentById("1") } returns attachment1
             every { messageDetailsRepository.findAttachmentById("2") } returns attachment2
 
-            val result = uploadAttachments(attachmentIds, message, crypto)
+            val result = uploadAttachments(attachmentIds, message, crypto, false)
 
             coVerifyOrder {
                 pendingActionsDao.findPendingUploadByMessageId("messageId8234")
@@ -157,7 +157,7 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Failure("Failed to upload attachment2")
             }
 
-            val result = uploadAttachments(attachmentIds, message, crypto)
+            val result = uploadAttachments(attachmentIds, message, crypto, false)
 
             val expected = UploadAttachments.Result.Failure("Failed to upload attachment2")
             assertEquals(expected, result)
@@ -177,10 +177,11 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Failure("Failed to upload public key")
             }
 
-            val result = uploadAttachments(attachmentIds, message, crypto)
+            val result = uploadAttachments(attachmentIds, message, crypto, true)
 
             val expected = UploadAttachments.Result.Failure("Failed to upload public key")
             assertEquals(expected, result)
+            coVerify { attachmentsRepository.uploadPublicKey(message, crypto) }
             verify { pendingActionsDao.deletePendingUploadByMessageId("messageId9273585") }
         }
     }
@@ -199,7 +200,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             every { messageDetailsRepository.findAttachmentById("1") } returns null
             every { messageDetailsRepository.findAttachmentById("2") } returns attachment2
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             coVerifySequence { attachmentsRepository.upload(attachment2, crypto) }
         }
@@ -225,7 +226,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             every { messageDetailsRepository.findAttachmentById("1") } returns attachment1
             every { messageDetailsRepository.findAttachmentById("2") } returns attachment2
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             coVerify(exactly = 0) { attachmentsRepository.upload(attachment1, crypto) }
             coVerify { attachmentsRepository.upload(attachment2, crypto) }
@@ -252,7 +253,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             every { messageDetailsRepository.findAttachmentById("1") } returns attachment1
             every { messageDetailsRepository.findAttachmentById("2") } returns attachment2
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             coVerify { attachmentsRepository.upload(attachment1, crypto) }
             coVerify(exactly = 0) { attachmentsRepository.upload(attachment2, crypto) }
@@ -279,7 +280,7 @@ class UploadAttachmentsTest : CoroutinesTest {
             every { messageDetailsRepository.findAttachmentById("1") } returns attachmentMock1
             every { messageDetailsRepository.findAttachmentById("2") } returns attachmentMock2
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             coVerify { attachmentsRepository.upload(attachmentMock1, crypto) }
             coVerify(exactly = 0) { attachmentsRepository.upload(attachmentMock2, crypto) }
@@ -287,7 +288,7 @@ class UploadAttachmentsTest : CoroutinesTest {
     }
 
     @Test
-    fun uploadAttachmentsCallRepositoryUploadPublicKeyWhenMailSettingsGetAttachPublicKeyIsTrue() {
+    fun uploadAttachmentsCallRepositoryUploadPublicKeyWhenMailSettingsGetAttachPublicKeyIsTrueAndMessageIsSending() {
         runBlockingTest {
             val username = "username"
             val message = Message("messageId823762")
@@ -297,7 +298,7 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Success("23421")
             }
 
-            uploadAttachments(emptyList(), message, crypto)
+            uploadAttachments(emptyList(), message, crypto, true)
 
             coVerify { attachmentsRepository.uploadPublicKey(message, crypto) }
         }
@@ -329,7 +330,7 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Failure("failed")
             }
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             verify { attachmentMock1.deleteLocalFile() }
             verify(exactly = 0) { attachmentMock2.deleteLocalFile() }
@@ -385,7 +386,7 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Success(uploadedAttachmentMock2Id)
             }
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             val actualMessage = slot<Message>()
             val expectedAttachments = listOf(uploadedAttachmentMock1, uploadedAttachmentMock2)
@@ -414,7 +415,7 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Failure("Failed uploading attachment!")
             }
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             verify(exactly = 1) { messageDetailsRepository.findAttachmentById("1") }
             coVerify(exactly = 0) { messageDetailsRepository.saveMessageLocally(any()) }
@@ -457,12 +458,52 @@ class UploadAttachmentsTest : CoroutinesTest {
                 AttachmentsRepository.Result.Success(uploadedAttachment2Id)
             }
 
-            uploadAttachments(attachmentIds, message, crypto)
+            uploadAttachments(attachmentIds, message, crypto, false)
 
             val actualMessage = slot<Message>()
             val expectedAttachments = listOf(attachmentMock1)
             coVerify(exactly = 2) { messageDetailsRepository.saveMessageLocally(capture(actualMessage)) }
             assertEquals(expectedAttachments, actualMessage.captured.Attachments)
+        }
+    }
+
+    @Test
+    fun uploadAttachmentsDoesNotAttachThePublicKeyIfTheMessageIsNotBeingSent() {
+        runBlockingTest {
+            val attachmentIds = emptyList<String>()
+            val messageId = "message-id-123842"
+            val message = Message(messageId)
+            val username = "username"
+            every { userManager.username } returns username
+            every { pendingActionsDao.findPendingUploadByMessageId(messageId) } returns null
+            every { userManager.getMailSettings(username)?.getAttachPublicKey() } returns true
+            coEvery { attachmentsRepository.uploadPublicKey(message, crypto) } returns mockk()
+
+            val result = uploadAttachments(attachmentIds, message, crypto, false)
+
+            coVerify(exactly = 0) { attachmentsRepository.uploadPublicKey(any(), any()) }
+            assertEquals(UploadAttachments.Result.Success, result)
+        }
+    }
+
+    @Test
+    fun uploadAttachmentsAttachesThePublicKeyOnlyWhenTheMessageIsBeingSent() {
+        runBlockingTest {
+            val attachmentIds = emptyList<String>()
+            val messageId = "message-id-123842"
+            val message = Message(messageId)
+            val username = "username"
+            every { userManager.username } returns username
+            every { pendingActionsDao.findPendingUploadByMessageId(messageId) } returns null
+            every { userManager.getMailSettings(username)?.getAttachPublicKey() } returns true
+            coEvery { attachmentsRepository.uploadPublicKey(message, crypto) } answers {
+                AttachmentsRepository.Result.Success("82384")
+            }
+
+            val result = uploadAttachments.blocking(attachmentIds, message, crypto, true)
+
+            coVerify { attachmentsRepository.uploadPublicKey(any(), any()) }
+            assertEquals(UploadAttachments.Result.Success, result)
         }
     }
 }

app/src/test/java/ch/protonmail/android/usecase/compose/SaveDraftTest.kt
@@ -246,7 +246,7 @@ class SaveDraftTest : CoroutinesTest {
                     "234234", localDraftId, "offlineId", false, 834L
                 )
             }
-            coEvery { uploadAttachments(any(), any(), any()) } returns UploadAttachments.Result.Success
+            coEvery { uploadAttachments(any(), any(), any(), false) } returns UploadAttachments.Result.Success
             val workOutputData = workDataOf(
                 KEY_OUTPUT_RESULT_SAVE_DRAFT_MESSAGE_ID to "createdDraftMessageId"
             )
@@ -294,7 +294,7 @@ class SaveDraftTest : CoroutinesTest {
             coEvery { messageDetailsRepository.findMessageById("createdDraftMessageId345") } returns apiDraft
             every { pendingActionsDao.findPendingSendByDbId(9833L) } returns null
             every { pendingActionsDao.findPendingSendByOfflineMessageId(localDraftId) } returns PendingSend()
-            coEvery { uploadAttachments(any(), apiDraft, any()) } returns UploadAttachments.Result.Success
+            coEvery { uploadAttachments(any(), apiDraft, any(), false) } returns UploadAttachments.Result.Success
             every {
                 createDraftScheduler.enqueue(
                     message,
@@ -312,7 +312,7 @@ class SaveDraftTest : CoroutinesTest {
             ).first()
 
             // Then
-            coVerify { uploadAttachments(newAttachmentIds, apiDraft, addressCrypto) }
+            coVerify { uploadAttachments(newAttachmentIds, apiDraft, addressCrypto, false) }
         }
     }
 
@@ -378,7 +378,7 @@ class SaveDraftTest : CoroutinesTest {
             coEvery { messageDetailsRepository.findMessageById("newDraftId") } returns message.copy(messageId = "newDraftId")
             coEvery { messageDetailsRepository.findMessageById("45623") } returns message
             every { pendingActionsDao.findPendingSendByDbId(9833L) } returns null
-            coEvery { uploadAttachments(newAttachmentIds, any(), any()) } returns Failure(errorMessage)
+            coEvery { uploadAttachments(newAttachmentIds, any(), any(), false) } returns Failure(errorMessage)
             every {
                 createDraftScheduler.enqueue(
                     message,
@@ -422,7 +422,7 @@ class SaveDraftTest : CoroutinesTest {
             coEvery { messageDetailsRepository.findMessageById("createdDraftMessageId345") } returns apiDraft
             every { pendingActionsDao.findPendingSendByDbId(9833L) } returns null
             every { pendingActionsDao.findPendingSendByOfflineMessageId(localDraftId) } returns PendingSend()
-            coEvery { uploadAttachments(any(), apiDraft, any()) } returns UploadAttachments.Result.Success
+            coEvery { uploadAttachments(any(), apiDraft, any(), false) } returns UploadAttachments.Result.Success
             every {
                 createDraftScheduler.enqueue(
                     message,