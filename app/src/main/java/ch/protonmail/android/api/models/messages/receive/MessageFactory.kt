9c8033e5ff67b855c631d28dd7432aeb4cee6687
Marino Meneghel <marino.meneghel@protonmail.com>
2021-02-24 10:39:38 +0100

    Format MessageFactory following detekt rules
    MAILAND-1555

--- app/src/main/java/ch/protonmail/android/api/models/messages/receive/MessageFactory.kt
+++ app/src/main/java/ch/protonmail/android/api/models/messages/receive/MessageFactory.kt
@@ -1,18 +1,18 @@
 /*
  * Copyright (c) 2020 Proton Technologies AG
- * 
+ *
  * This file is part of ProtonMail.
- * 
+ *
  * ProtonMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ProtonMail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
  */
@@ -84,17 +84,20 @@ class MessageFactory @Inject constructor(
                 .filter { it.length <= 2 }
                 .map { it.toInt() }
                 .fold(Constants.MessageLocationType.ALL_MAIL.messageLocationTypeValue) { location, newLocation ->
-                    if (newLocation !in listOf(
-                            Constants.MessageLocationType.STARRED.messageLocationTypeValue,
-                            Constants.MessageLocationType.ALL_MAIL.messageLocationTypeValue,
-                            Constants.MessageLocationType.INVALID.messageLocationTypeValue
-                        ) && newLocation < location
+                    if (
+                        newLocation !in listOf(
+                                Constants.MessageLocationType.STARRED.messageLocationTypeValue,
+                                Constants.MessageLocationType.ALL_MAIL.messageLocationTypeValue,
+                                Constants.MessageLocationType.INVALID.messageLocationTypeValue
+                            ) &&
+                        newLocation < location
                     ) {
                         newLocation
-                    } else if (newLocation in listOf(
-                            Constants.MessageLocationType.DRAFT.messageLocationTypeValue,
-                            Constants.MessageLocationType.SENT.messageLocationTypeValue
-                        )
+                    } else if (
+                        newLocation in listOf(
+                                Constants.MessageLocationType.DRAFT.messageLocationTypeValue,
+                                Constants.MessageLocationType.SENT.messageLocationTypeValue
+                            )
                     ) {
                         newLocation
                     } else
