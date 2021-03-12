9078db0d8defcca9697fea05ae9ba00971f5b6a5
Zorica Stojchevska <zorica@protonmail.com>
2021-03-12 12:42:41 +0100

    Changed build version
    #comment Just increased the build version code for a new beta
    Affected: nothing

--- buildSrc/src/main/kotlin/ProtonMail.kt
+++ buildSrc/src/main/kotlin/ProtonMail.kt
@@ -23,7 +23,7 @@
  */
 object ProtonMail {
     const val versionName = "1.13.28"
-    const val versionCode = 763
+    const val versionCode = 764
 
     const val targetSdk = 30
     const val minSdk = 21
