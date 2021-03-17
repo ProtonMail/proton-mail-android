f3745a8f8dfc32dad3af8678756c136faa564fa2
Davide Farella <4face91@protonmail.com>
2021-03-17 12:30:09 +0100

    Migrate exisiting jUnit5 tests to jUnit4
    This decision was taken based on the fact that jUnit5 is currently not
    officially supported on android and at now we're not using any on its
    features.
    We'll stay on jUnit4 for the moment being and come back to this later on
    if there's a need
    
    MAILAND-1342
    
    # Conflicts:
    #	app/src/test/java/ch/protonmail/android/utils/extensions/StringExtensionsTest.kt
    
    # Conflicts:
    #	app/src/androidTest/java/ch/protonmail/android/crypto/CryptoTest.kt
    #	app/src/test/java/ch/protonmail/android/activities/messageDetails/IntentExtrasDataTest.kt
    #	app/src/test/java/ch/protonmail/android/api/local/SnoozeSettingsTest.kt
    #	app/src/test/java/ch/protonmail/android/api/models/contacts/receive/ContactLabelFactoryTest.kt
    #	app/src/test/java/ch/protonmail/android/api/models/messages/receive/ServerMessageTest.kt
    #	app/src/test/java/ch/protonmail/android/api/models/room/messages/MessagesTypesConverterTest.kt
    #	app/src/test/java/ch/protonmail/android/attachments/AttachmentsRepositoryTest.kt
    #	app/src/test/java/ch/protonmail/android/attachments/UploadAttachmentsTest.kt
    #	app/src/test/java/ch/protonmail/android/contacts/details/ContactDetailsRepositoryTest.kt
    #	app/src/test/java/ch/protonmail/android/contacts/details/ContactGroupsRepositoryTest.kt
    #	app/src/test/java/ch/protonmail/android/contacts/groups/edit/ContactGroupEditCreateRepositoryTest.kt
    #	app/src/test/java/ch/protonmail/android/utils/extensions/CollectionExtensionsTest.kt
    #	app/src/test/java/ch/protonmail/android/worker/CreateContactGroupWorkerTest.kt
    #	app/src/test/java/ch/protonmail/android/worker/CreateContactWorkerTest.kt
    #	app/src/test/java/ch/protonmail/android/worker/PostLabelWorkerTest.kt
    
    # Conflicts:
    #	app/src/test/java/ch/protonmail/android/attachments/AttachmentsRepositoryTest.kt

--- app/src/test/java/ch/protonmail/android/attachments/AttachmentsRepositoryTest.kt
+++ app/src/test/java/ch/protonmail/android/attachments/AttachmentsRepositoryTest.kt
@@ -76,7 +76,7 @@ class AttachmentsRepositoryTest : CoroutinesTest {
     @InjectMockKs
     private lateinit var repository: AttachmentsRepository
 
-    @BeforeTest
+    @Before
     fun setUp() {
         MockKAnnotations.init(this)
         val successResponse = mockk<AttachmentUploadResponse> {
