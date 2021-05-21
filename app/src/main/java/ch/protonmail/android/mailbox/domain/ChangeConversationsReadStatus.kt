7236bd08ebe8b26387be4e78d8f334b7478e725b
stefanija <stefanija.boshkovska@protonmail.com>
2021-05-21 17:15:57 +0200

    Add ChangeConversationsReadStatus use case
    MAILAND-1438

---
+++ app/src/main/java/ch/protonmail/android/mailbox/domain/ChangeConversationsReadStatus.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ *
+ * This file is part of ProtonMail.
+ *
+ * ProtonMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+ */
+
+package ch.protonmail.android.mailbox.domain
+
+import ch.protonmail.android.core.Constants
+import ch.protonmail.android.domain.entity.Id
+import javax.inject.Inject
+
+/**
+ * A use case that handles changing the read/unread status of a conversation
+ */
+class ChangeConversationsReadStatus @Inject constructor(
+    private val conversationsRepository: ConversationsRepository
+) {
+
+    suspend operator fun invoke(
+        conversationIds: List<String>,
+        action: Action,
+        userId: Id,
+        location: Constants.MessageLocationType
+    ) {
+        if (action == Action.ACTION_MARK_READ) {
+            conversationsRepository.markRead(conversationIds)
+        } else {
+            conversationsRepository.markUnread(conversationIds, userId, location)
+        }
+    }
+
+    enum class Action {
+        ACTION_MARK_READ,
+        ACTION_MARK_UNREAD
+    }
+}
---
+++ app/src/test/java/ch/protonmail/android/mailbox/domain/ChangeConversationsReadStatusTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ *
+ * This file is part of ProtonMail.
+ *
+ * ProtonMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+ */
+
+package ch.protonmail.android.mailbox.domain
+
+import ch.protonmail.android.core.Constants
+import ch.protonmail.android.domain.entity.Id
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.runs
+import kotlinx.coroutines.test.runBlockingTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+/**
+ * Test the behavior of [ChangeConversationsReadStatus]
+ */
+class ChangeConversationsReadStatusTest {
+
+    private val conversationsRepository = mockk<ConversationsRepository>()
+
+    private lateinit var changeConversationsReadStatus: ChangeConversationsReadStatus
+
+    @BeforeTest
+    fun setUp() {
+        changeConversationsReadStatus = ChangeConversationsReadStatus(
+            conversationsRepository
+        )
+    }
+
+    @Test
+    fun verifyMarkReadIsCalledWhenReadActionIsReceived() {
+        runBlockingTest {
+            // given
+            val conversation1 = "conversation1"
+            val conversation2 = "conversation2"
+            val conversationIds = listOf(conversation1, conversation2)
+            coEvery { conversationsRepository.markRead(conversationIds) } just runs
+
+            // when
+            changeConversationsReadStatus(
+                conversationIds,
+                ChangeConversationsReadStatus.Action.ACTION_MARK_READ,
+                Id("id"),
+                Constants.MessageLocationType.ARCHIVE
+            )
+
+            // then
+            coVerify {
+                conversationsRepository.markRead(conversationIds)
+            }
+        }
+    }
+
+    @Test
+    fun verifyMarkUnreadIsCalledWhenUnreadActionIsReceived() {
+        runBlockingTest {
+            // given
+            val conversation1 = "conversation1"
+            val conversation2 = "conversation2"
+            val conversationIds = listOf(conversation1, conversation2)
+            coEvery { conversationsRepository.markUnread(conversationIds, any(), any()) } just runs
+
+            // when
+            changeConversationsReadStatus(
+                conversationIds,
+                ChangeConversationsReadStatus.Action.ACTION_MARK_UNREAD,
+                Id("id"),
+                Constants.MessageLocationType.ARCHIVE
+            )
+
+            // then
+            coVerify {
+                conversationsRepository.markUnread(conversationIds, any(), any())
+            }
+        }
+    }
+}
