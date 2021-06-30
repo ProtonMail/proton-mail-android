27b1614a0e0bf70de9d345cdb2895262b6019b69
Davide Farella <4face91@protonmail.com>
2021-06-30 15:26:53 +0200

    Improve FieldsTest.kt style
    Remove assert4k and renamed tests following camelCase rule
    
    MAILAND-2011

--- domain/src/test/kotlin/ch/protonmail/android/domain/entity/FieldsTest.kt
+++ domain/src/test/kotlin/ch/protonmail/android/domain/entity/FieldsTest.kt
@@ -1,25 +1,25 @@
 /*
  * Copyright (c) 2020 Proton Technologies AG
- * 
+ *
  * This file is part of ProtonMail.
- * 
+ *
  * ProtonMail is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * ProtonMail is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
  */
 package ch.protonmail.android.domain.entity
 
-import assert4k.*
 import kotlin.test.Test
+import kotlin.test.assertFails
 
 /**
  * Test suite for [EmailAddress]
@@ -28,7 +28,7 @@ import kotlin.test.Test
 internal class FieldsTest {
 
     @Test
-    fun `verify happy paths`() {
+    fun verifyHappyPaths() {
         listOf(
             "somebody@protonmail.com",
             "mail@4cafe.diostu",
@@ -38,20 +38,20 @@ internal class FieldsTest {
     }
 
     @Test
-    fun `verify failing paths`() {
-        assert that fails<ValidationException> {
+    fun verifyFailingPaths() {
+        assertFails {
             EmailAddress("@hello.com")
         }
-        assert that fails<ValidationException> {
+        assertFails {
             EmailAddress("hello.com")
         }
-        assert that fails<ValidationException> {
+        assertFails {
             EmailAddress("hello@com")
         }
-        assert that fails<ValidationException> {
+        assertFails {
             EmailAddress("hello@.com")
         }
-        assert that fails<ValidationException> {
+        assertFails {
             EmailAddress("hello@-.com")
         }
     }
