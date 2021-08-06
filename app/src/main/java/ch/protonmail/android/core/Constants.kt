6bf310349cfa1fcbb02b110543a848fd48a5330e
Marino Meneghel <marino.meneghel@protonmail.com>
2021-08-06 17:24:06 +0200

    Revert "Remove `DUMMY_URL_PREFIX` as "http://androidlinksfix.protonmail.com" from MessageDetailsAdapter.kt"
    This reverts commit 88de93ca18aa8d9456859249d19b5347d458209a.
    
    Commit 88de93 was an attempt to fix MAILAND-1801, but dropping the
    "DUMMY_URL_PREFIX" had some unintended side effects.
    
    One is that the `webview.reload` call performed to load a message's
    remote content stops working properly, reloading to a blank message
    instead of reloading the existing HTML content.
    
    This bad behavior reloading only happens when the history of a
    message was being displayed (MAILAND-2220).
    
    That seem to happen because the default url for `loadDataWithBaseURL` is
    "about:blank"
    
    MAILAND-2220

--- app/src/main/java/ch/protonmail/android/core/Constants.kt
+++ app/src/main/java/ch/protonmail/android/core/Constants.kt
@@ -28,6 +28,7 @@ object Constants {
     // region Urls
     const val API_HOST = "api.protonmail.ch"
     const val BASE_URL = "https://$API_HOST"
+    const val DUMMY_URL_PREFIX = "http://androidlinksfix.protonmail.com"
 
     // Mail domains
     const val MAIL_DOMAIN_CH = "protonmail.ch"
