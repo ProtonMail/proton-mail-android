b7c899161d60dcff4569b4ff63e0e7247fbf9462
Marino Meneghel <marino.meneghel@protonmail.com>
2021-09-13 17:38:54 +0200

    Convert MessageEncryption enum to kotlin
    MAILAND-1891

---
+++ app/src/main/java/ch/protonmail/android/api/models/enumerations/MessageEncryption.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * 
+ * This file is part of ProtonMail.
+ * 
+ * ProtonMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * ProtonMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+ */
+package ch.protonmail.android.api.models.enumerations
+
+enum class MessageEncryption(
+    /**
+     * Drafts are also seen as end-to-end encrypted as drafts are never leaked to the server.
+     */
+    val isEndToEndEncrypted: Boolean,
+    val isStoredEncrypted: Boolean,
+    val isInternalEncrypted: Boolean,
+    val isPGPEncrypted: Boolean
+) {
+
+    INTERNAL(
+        isEndToEndEncrypted = true,
+        isStoredEncrypted = true,
+        isInternalEncrypted = true,
+        isPGPEncrypted = false
+    ),
+    EXTERNAL(
+        isEndToEndEncrypted = false,
+        isStoredEncrypted = true,
+        isInternalEncrypted = false,
+        isPGPEncrypted = false
+    ),
+    EXTERNAL_PGP(
+        isEndToEndEncrypted = false,
+        isStoredEncrypted = true,
+        isInternalEncrypted = false,
+        isPGPEncrypted = true
+    ),
+    MIME_PGP(
+        isEndToEndEncrypted = true,
+        isStoredEncrypted = true,
+        isInternalEncrypted = false,
+        isPGPEncrypted = true
+    ),
+    AUTO_RESPONSE(
+        isEndToEndEncrypted = false,
+        isStoredEncrypted = true,
+        isInternalEncrypted = true,
+        isPGPEncrypted = false
+    );
+
+}
