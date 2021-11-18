2239941204d344b93a744798048425fb0f773377
Zorica Stojchevska <zorica@protonmail.com>
2021-11-19 00:35:31 +0100

    Change how we resolve if attachment isUploaded or not
    Now in order to consider an attachment as uploaded we check the
    `attachmentId` if it's not an integer then that attachment has been uploaded.

--- app/src/main/java/ch/protonmail/android/data/local/model/Attachment.kt
+++ app/src/main/java/ch/protonmail/android/data/local/model/Attachment.kt
@@ -29,6 +29,7 @@ import androidx.room.Index
 import androidx.room.PrimaryKey
 import ch.protonmail.android.data.local.MessageDao
 import ch.protonmail.android.utils.AppUtil
+import ch.protonmail.android.utils.MessageUtils.isLocalAttachmentId
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import java.io.ByteArrayOutputStream
@@ -86,7 +87,7 @@ data class Attachment constructor(
     var messageId: String = "",
 
     @ColumnInfo(name = COLUMN_ATTACHMENT_UPLOADED)
-    var isUploaded: Boolean = false,
+    var isUploaded: Boolean = !isLocalAttachmentId(attachmentId),
 
     @ColumnInfo(name = COLUMN_ATTACHMENT_UPLOADING)
     var isUploading: Boolean = false,
@@ -222,7 +223,6 @@ data class Attachment constructor(
                 keyPackets = localAttachment.keyPackets,
                 headers = localAttachment.headers
             )
-
         }
 
         /**
@@ -230,9 +230,9 @@ data class Attachment constructor(
          * @throws NoSuchAlgorithmException
          * @throws IOException
          */
-        fun fromMimeAttachment(part: Part, message_id: String, count: Int): Attachment {
+        fun fromMimeAttachment(part: Part, messageId: String, count: Int): Attachment {
             val data = getBytesFromInputStream(part.inputStream)
-            return fromMimeAttachment(part, data, message_id, count)
+            return fromMimeAttachment(part, data, messageId, count)
         }
 
         /**
@@ -240,7 +240,7 @@ data class Attachment constructor(
          * @throws NoSuchAlgorithmException
          * @throws IOException
          */
-        fun fromMimeAttachment(part: Part, data: ByteArray, message_id: String, count: Int): Attachment {
+        fun fromMimeAttachment(part: Part, data: ByteArray, messageId: String, count: Int): Attachment {
             val attachment = Attachment()
 
             attachment.fileName = part.fileName ?: "default"
@@ -264,12 +264,12 @@ data class Attachment constructor(
                 md5.digest().forEach { b ->
                     format.format("%02x", b)
                 }
-                attachment.attachmentId = "PGPAttachment/$message_id/$format/$count"
+                attachment.attachmentId = "PGPAttachment/$messageId/$format/$count"
             }
             attachment.fileSize = data.size.toLong()
             attachment.mimeType = part.contentType.replace("(\\s*)?[\\r\\n]+(\\s*)".toRegex(), "")
             attachment.mimeData = data
-            attachment.messageId = message_id
+            attachment.messageId = messageId
             return attachment
         }
 
@@ -281,10 +281,10 @@ data class Attachment constructor(
         fun fromMimeAttachment(
             data: ByteArray,
             headers: InternetHeaders,
-            message_id: String,
+            messageId: String,
             count: Int
         ): Attachment =
-            fromMimeAttachment(MimeBodyPart(headers, ByteArray(0)), data, message_id, count)
+            fromMimeAttachment(MimeBodyPart(headers, ByteArray(0)), data, messageId, count)
 
         /**
          * @throws IOException
