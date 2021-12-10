a9117ae1001927b11f5c48ed61af22e532648b83
Davide Farella <4face91@protonmail.com>
2021-12-10 16:18:10 +0100

    Add GetAppThemeSettingsTest.kt and SaveAppThemeSettingsTest.kt
    MAILAND-2637

---
+++ app/src/test/java/ch/protonmail/android/settings/domain/usecase/GetAppThemeSettingsTest.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ *
+ * This file is part of ProtonMail.
+ *
+ * ProtonMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonMail. If not, see https://www.gnu.org/licenses/.
+ */
+
+package ch.protonmail.android.settings.domain.usecase
+
+import ch.protonmail.android.settings.domain.DeviceSettingsRepository
+import ch.protonmail.android.settings.domain.model.AppThemeSettings
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runBlockingTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class GetAppThemeSettingsTest {
+
+    private val repository: DeviceSettingsRepository = mockk()
+    private val getAppThemeSettings = GetAppThemeSettings(repository)
+
+    @Test
+    fun `returns correct data from the repository`() = runBlockingTest {
+        // given
+        val expected = AppThemeSettings.DARK
+        coEvery { repository.getAppThemeSettings() } returns expected
+
+        // when
+        val result = getAppThemeSettings()
+
+        // then
+        assertEquals(expected, result)
+    }
+}
