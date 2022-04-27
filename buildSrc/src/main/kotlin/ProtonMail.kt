5acc471e616ee29be346c300df7602c7d64c1b0d
Maciej Surmacz <maciej.surmacz@proton.ch>
2022-04-27 16:02:23 +0200

    Changed build version

--- buildSrc/src/main/kotlin/ProtonMail.kt
+++ buildSrc/src/main/kotlin/ProtonMail.kt
@@ -24,7 +24,7 @@
 object ProtonMail {
 
     const val versionName = "3.0.0"
-    const val versionCode = 838
+    const val versionCode = 839
 
     const val targetSdk = 31
     const val minSdk = 23
