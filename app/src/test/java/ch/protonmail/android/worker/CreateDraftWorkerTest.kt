fe2683716bbb8a74adaf04471410d220a93840df
Zorica Stojchevska <zorica@protonmail.com>
2023-01-23 09:42:49 +0100

    Don't save body of message echoed from BE in DB while composing
    The purpose of this change is to keep the composer as source of truth.
    
    MAILAND-3028

--- app/src/test/java/ch/protonmail/android/worker/CreateDraftWorkerTest.kt
+++ app/src/test/java/ch/protonmail/android/worker/CreateDraftWorkerTest.kt
@@ -892,6 +892,7 @@ class CreateDraftWorkerTest : CoroutinesTest by CoroutinesTest() {
                 this.attachments = responseMessage.attachments
                 this.localId = message.messageId
                 this.expirationTime = message.expirationTime
+                this.messageBody = message.messageBody
             }
             val actualMessage = slot<Message>()
             coVerify { messageDetailsRepository.saveMessage(capture(actualMessage)) }
@@ -1149,6 +1150,7 @@ class CreateDraftWorkerTest : CoroutinesTest by CoroutinesTest() {
                 this.numAttachments = message.numAttachments
                 this.attachments = responseMessage.attachments
                 this.localId = message.messageId
+                this.messageBody = message.messageBody
             }
             val actualMessage = slot<Message>()
             coVerify { messageDetailsRepository.saveMessage(capture(actualMessage)) }
@@ -1222,6 +1224,7 @@ class CreateDraftWorkerTest : CoroutinesTest by CoroutinesTest() {
                 this.numAttachments = localMessage.numAttachments
                 this.attachments = listOf(localAttachment, remoteAttachment)
                 this.localId = localMessage.messageId
+                this.messageBody = localMessage.messageBody
             }
             val actualMessage = slot<Message>()
             coVerify { messageDetailsRepository.saveMessage(capture(actualMessage)) }
