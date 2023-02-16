643050bc0126bff003fa58504547c779b0b83d7d
Neil Marietta <neil.marietta@protonmail.com>
2023-02-16 16:24:16 +0100

    fix: Fixed foreign key reference to be renamed while recreating/altering target table.

--- buildSrc/src/main/kotlin/versionsConfig.kt
+++ buildSrc/src/main/kotlin/versionsConfig.kt
@@ -62,7 +62,7 @@ fun initVersions() {
 }
 
 // Proton Core
-const val `Proton-core version` =               "9.9.1"
+const val `Proton-core version` =               "9.9.2"
 
 // Test
 const val `aerogear version` =                  "1.0.0"         // Released: Mar 23, 2013
