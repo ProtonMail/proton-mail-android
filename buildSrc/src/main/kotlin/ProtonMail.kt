983828438d8944cf5dd7c3e40ba245342a610a9f
Zorica Stojchevska <zstojche@proton.ch>
2023-05-10 10:36:44 +0200

    Changed build version for a new release

--- buildSrc/src/main/kotlin/ProtonMail.kt
+++ buildSrc/src/main/kotlin/ProtonMail.kt
@@ -25,8 +25,8 @@ import org.gradle.api.JavaVersion
  */
 object ProtonMail {
 
-    const val versionName = "3.0.14"
-    const val versionCode = 936
+    const val versionName = "3.0.15"
+    const val versionCode = 937
 
     const val compileSdk = 33
     const val targetSdk = 31
